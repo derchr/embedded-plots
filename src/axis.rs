7790a4fa4dfd40609bb43791d16528d31962a2fd
Michał Chodzikiewicz <mchodzikiewicz@gmail.com>
2020-12-31 00:04:24 +0100

    Move text style building out of Axis

--- src/axis.rs
+++ src/axis.rs
@@ -3,9 +3,9 @@ use embedded_graphics::drawable::Drawable;
 use embedded_graphics::DrawTarget;
 use core::ops::Range;
 use embedded_graphics::prelude::*;
-use embedded_graphics::style::{PrimitiveStyle, TextStyleBuilder};
+use embedded_graphics::style::{PrimitiveStyle, TextStyle};
 use crate::range_conv::Scalable;
-use embedded_graphics::fonts::{Text, Font6x8};
+use embedded_graphics::fonts::Text;
 use heapless::{consts::*, String};
 use core::fmt::Write;
 
@@ -27,27 +27,37 @@ pub enum Scale {
     RangeFraction(usize),
 }
 
-pub struct Axis<'a, C> {
+pub struct Axis<'a, C, F>
+    where
+        C: PixelColor,
+        F: Font,
+        TextStyle<C,F>: Clone,
+{
     title: &'a str,
     orientation: Orientation,
     range: Range<i32>,
     scale: Scale,
     color: C,
+    text_style: TextStyle<C,F>
 }
 
-impl<'a, C> Axis<'a, C>
+impl<'a, C, F> Axis<'a, C, F>
     where
         C: PixelColor,
+        F: Font,
+        TextStyle<C,F>: Clone,
 {
-    pub fn new(title: &'a str, orientation: Orientation, range: Range<i32>, scale: Scale, color: C) -> Axis<'a, C> {
-        Axis { title, orientation, range, scale, color }
+    pub fn new(title: &'a str, orientation: Orientation, range: Range<i32>, scale: Scale, color: C, text_style: TextStyle<C,F>) -> Axis<'a, C, F> {
+        Axis { title, orientation, range, scale, color, text_style }
     }
 }
 
 
-impl<'a, C> Drawable<C> for Axis<'a, C>
+impl<'a, C, F> Drawable<C> for Axis<'a, C, F>
     where
         C: PixelColor,
+        F: Font + Copy,
+        TextStyle<C,F>: Clone,
 {
     fn draw<D: DrawTarget<C>>(self, display: &mut D) -> Result<(), D::Error> {
         let lines = match self.scale {
@@ -59,17 +69,13 @@ impl<'a, C> Drawable<C> for Axis<'a, C>
                 self.range.clone().into_iter().step_by(len / fraction)
             }
         };
-        // Create a new text style
-        let style = TextStyleBuilder::new(Font6x8)
-            .text_color(self.color)
-            .build();
         match self.orientation {
             Orientation::X { x1, x2, y } => {
                 Line { start: Point { x: x1, y }, end: Point { x: x2, y } }
                     .into_styled(PrimitiveStyle::with_stroke(self.color, 1))
                     .draw(display)?;
                 let title = Text::new(self.title, Point { x: x1, y: y + 10 })
-                    .into_styled(style);
+                    .into_styled(self.text_style);
                 let title = title.translate(Point { x: (x2 - x1) / 2 - title.size().width as i32 / 2, y: 0 });
                 title.draw(display)?;
 
@@ -80,7 +86,7 @@ impl<'a, C> Drawable<C> for Axis<'a, C>
                         .draw(display)?;
                     let mut buf: String::<U8> = String::new();
                     write!(buf, "{}", line).unwrap();
-                    Text::new(&buf, Point { x: x + 1, y: y + 1 }).into_styled(style).draw(display)?;
+                    Text::new(&buf, Point { x: x + 1, y: y + 1 }).into_styled(self.text_style).draw(display)?;
                 }
             }
             Orientation::Y { y1, y2, x } => {
@@ -88,7 +94,7 @@ impl<'a, C> Drawable<C> for Axis<'a, C>
                     .into_styled(PrimitiveStyle::with_stroke(self.color, 1))
                     .draw(display)?;
                 let title = Text::new(self.title, Point { x, y: y1 })
-                    .into_styled(style);
+                    .into_styled(self.text_style);
                 let title = title.translate(Point { x: -(title.size().width as i32) - 5, y: (y2-y1)/2 });
                 title.draw(display)?;
 
@@ -99,7 +105,7 @@ impl<'a, C> Drawable<C> for Axis<'a, C>
                         .draw(display)?;
                     let mut buf: String::<U8> = String::new();
                     write!(buf, "{}", line).unwrap();
-                    let tick = Text::new(&buf, Point { x, y}).into_styled(style);
+                    let tick = Text::new(&buf, Point { x, y}).into_styled(self.text_style);
                     let tick = tick.translate(Point{ x: -(tick.size().width as i32), y: 0 });
                     tick.draw(display)?;
                 }
